858c30baa9d8b9f1d0846d8b17cfbc8a2ca0452d
yangpeicheng <18362903155@163.com>
2017-10-29 23:06:03 +0800

    update

--- myDML/myDML.py
+++ myDML/myDML.py
@@ -9,10 +9,11 @@ import numpy as np
 import time
 from threading import Thread
 import functools
+import math
 
 # (global) variable definition here
 TRAINING_TIME_LIMIT = 60*10
-
+global A,distanceMatrix,rawData,label,label2indexs
 # class definition here
 
 # function definition here
@@ -43,18 +44,59 @@ def timeout(timeout):
 
 @timeout(TRAINING_TIME_LIMIT)
 def train(traindata):
-    rawData=traindata[0]
-    label=traindata[1]
+    init(traindata)
+    print(updateDistanceMatrix())
     # 在此处完成你的训练函数，注意训练时间不要超过TRAINING_TIME_LIMIT(秒)。
     time.sleep(1) # 这行仅用于测试训练超时，运行时请删除这行，否则你的TRAINING_TIME_LIMIT将-1s。
     return 0
 
+def init(traindata):
+    global rawData,A,label,distanceMatrix
+    rawData=traindata[0]
+    label=traindata[1]
+    A=np.eye(rawData.shape[1])
+    distanceMatrix = np.zeros((len(label), len(label)))
+    classifyLabel(label)
+
 def Euclidean_distance(inst_a, inst_b):
     return np.linalg.norm(inst_a - inst_b)
 
-def distance(inst_a, inst_b):
-    dist = np.random.rand() # 随机度量 这是一个错误示范，请删除这行 _(:3 」∠)_
-    return dist
+def distance(inst_a, inst_b):           #行向量
+    dist = np.dot(A,(inst_a-inst_b).transpose())
+    return np.dot(dist.transpose(),dist)    #(A(x-y))T(A(x-y))
+
+def classifyLabel(label):
+    global label2indexs
+    label2indexs={}
+    for i in range(len(label)):
+        if label[i] in label2indexs.keys():
+            label2indexs[label[i]].append(i)
+        else:
+            label2indexs[label[i]]=[i]
+    return label2indexs
+
+def updateDistanceMatrix():
+    global distanceMatrix,rawData
+    for i in range(len(rawData)):
+        for j in range(i+1):
+            distanceMatrix[i,j]=math.exp(-distance(rawData[i],rawData[j]))
+            distanceMatrix[j,i]=distanceMatrix[i,j]
+    return distanceMatrix
+
+def Pij(i,j):
+    global distanceMatrix
+    return distanceMatrix[i,j]/(sum(distanceMatrix[i,:])-1)
+
+def Pi(i):
+    global distanceMatrix,label,label2indexs
+    indexs=label2indexs[label[i]]
+    pi=0
+    for j in indexs:
+        pi+=Pij(i,j)
+    return pi
+
+def deltaA():
+
 
 # main program here
 if  __name__ == '__main__':
