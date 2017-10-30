6e1bc0b268f947c5f493ff6d8193d24be74adb87
yangpeicheng <18362903155@163.com>
2017-10-30 20:30:15 +0800

    update

--- myDML/myDML.py
+++ myDML/myDML.py
@@ -13,7 +13,7 @@ import math
 
 # (global) variable definition here
 TRAINING_TIME_LIMIT = 60*10
-global A,distanceMatrix,rawData,label,label2indexs
+global A,distanceMatrix,rawData,label,label2indexs,normValue
 # class definition here
 
 # function definition here
@@ -44,26 +44,37 @@ def timeout(timeout):
 
 @timeout(TRAINING_TIME_LIMIT)
 def train(traindata):
+    global A
     init(traindata)
-    print(updateDistanceMatrix())
+    lastfa=-100
+    for i in range(50):
+        print(i)
+        updateDistanceMatrix()
+        A=A+0.03*deltaA()
+        print(A)
+        currentfa=fA()
+        print(currentfa-lastfa)
+        lastfa=currentfa
     # 在此处完成你的训练函数，注意训练时间不要超过TRAINING_TIME_LIMIT(秒)。
-    time.sleep(1) # 这行仅用于测试训练超时，运行时请删除这行，否则你的TRAINING_TIME_LIMIT将-1s。
+    print(A)
     return 0
 
 def init(traindata):
-    global rawData,A,label,distanceMatrix
+    global rawData,A,label,distanceMatrix,normValue
     rawData=traindata[0]
     label=traindata[1]
-    A=np.eye(rawData.shape[1])
+    A=np.random.rand(rawData.shape[1],rawData.shape[1])
     distanceMatrix = np.zeros((len(label), len(label)))
+    normValue=[0 for i in range(len(label))]
     classifyLabel(label)
 
 def Euclidean_distance(inst_a, inst_b):
     return np.linalg.norm(inst_a - inst_b)
 
 def distance(inst_a, inst_b):           #行向量
-    dist = np.dot(A,(inst_a-inst_b).transpose())
-    return np.dot(dist.transpose(),dist)    #(A(x-y))T(A(x-y))
+    transformed_a=np.dot(A,inst_a.transpose())
+    transformed_b=np.dot(A,inst_b.transpose())
+    return Euclidean_distance(transformed_a,transformed_b)    #(A(x-y))T(A(x-y))
 
 def classifyLabel(label):
     global label2indexs
@@ -76,16 +87,21 @@ def classifyLabel(label):
     return label2indexs
 
 def updateDistanceMatrix():
-    global distanceMatrix,rawData
-    for i in range(len(rawData)):
+    global distanceMatrix,rawData,normValue
+    l=len(rawData)
+    for i in range(l):
         for j in range(i+1):
-            distanceMatrix[i,j]=math.exp(-distance(rawData[i],rawData[j]))
+            distanceMatrix[i,j]=math.exp(-math.pow(distance(rawData[i],rawData[j]),2))
             distanceMatrix[j,i]=distanceMatrix[i,j]
+    for i in range(l):
+        normValue[i]=sum(distanceMatrix[i,:])-1
     return distanceMatrix
 
 def Pij(i,j):
-    global distanceMatrix
-    return distanceMatrix[i,j]/(sum(distanceMatrix[i,:])-1)
+    global distanceMatrix,normValue
+    if i==j:
+        return 0
+    return distanceMatrix[i,j]/normValue[i]
 
 def Pi(i):
     global distanceMatrix,label,label2indexs
@@ -96,8 +112,28 @@ def Pi(i):
     return pi
 
 def deltaA():
+    global label,label2indexs,A
+    l=len(label)
+    s=np.zeros(A.shape)
+    for i in range(l):
+        s1=np.zeros(s.shape)
+        s2=np.zeros(s.shape)
+        for k in range(l):
+            xik=rawData[i]-rawData[k]
+            s1+=Pij(i,k)*np.dot(xik.transpose(),xik)
+        indexOfj=label2indexs[label[i]]
+        for j in indexOfj:
+            xij=rawData[i]-rawData[j]
+            s2+=Pij(i,j)*np.dot(xij.transpose(),xij)
+        s+=Pi(i)*s1-s2
+    return 2*s*A
 
-
+def fA():
+    global label
+    fa=0
+    for i in range(len(label)):
+        fa+=Pi(i)
+    return fa
 # main program here
 if  __name__ == '__main__':
     pass
